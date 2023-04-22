50406c31b7520e2d8a97c81948ed9390103a028c
Lokeshwar Deb <lokeshwardebdevelop@gmail.com>
2023-04-23 04:40:17 +0600

    developing the site and added the search tags sidebars and all

--- index.php
+++ index.php
@@ -1,24 +1,155 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jai sri ramkrishna</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <div class="container-fluid">
         <div class="row">
-<div class="col-4">Hi</div>
-<div class="col-8">video</div>
+            <!-- sidebars starts here -->
+            <div class="col-2">
+                <div class="container-fluid">
+                    <!-- logo starts here -->
+                    <div class="logo">
+                        <img src="img/logo.png" alt="" srcset="" class="img-fluid" width="100px"><sup> BD</sup>
+                    </div>
+                    <!-- logo ends here -->
+
+                    <!-- side link option starts here -->
+                    <div class="container-fluid">
+                        <div class="side_list_option d-flex">
+                        <tp-yt-paper-item class="d-flex style-scope ytd-guide-entry-renderer" role="tab" style-target="host" tabindex="0" aria-disabled="false" aria-selected="true" style="width: 100%; background: white; "><!--css-build:shady-->
+    <yt-icon class="guide-icon style-scope ytd-guide-entry-renderer"><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="pointer-events: none; display: block; width: auto; height: 100%;"><g class="style-scope yt-icon"><path d="M4,10V21h6V15h4v6h6V10L12,3Z" class="style-scope yt-icon"></path></g></svg><!--css-build:shady--></yt-icon>
+    <yt-img-shadow height="24" width="24" class="style-scope ytd-guide-entry-renderer" disable-upgrade="" hidden="">
+    </yt-img-shadow>
+    <yt-formatted-string class="title style-scope ytd-guide-entry-renderer mt-2">Home</yt-formatted-string>
+    <span class="guide-entry-count style-scope ytd-guide-entry-renderer">
+      
+    </span>
+    <yt-icon class="guide-entry-badge style-scope ytd-guide-entry-renderer" size="16" disable-upgrade="">
+    </yt-icon>
+    <div id="newness-dot" class="style-scope ytd-guide-entry-renderer"></div>
+  
+</tp-yt-paper-item>
+                            <!-- <ul>
+                                <li class="active rounded-border border"><a href="" class="">Home</a></li>
+                                <li><a href="">Home</a></li>
+                                <li><a href="">Home</a></li>
+                                <li><a href="">Home</a></li>
+                                <li><a href="">Home</a></li>
+                            </ul> -->
+                        </div>
+
+                    </div>
+                    <!-- side link option ends here -->
+
+
+                </div>
+            </div>
+
+            <!-- sidebars ends here -->
+
+            <!-- videos container starts here -->
+            <div class="col-10">
+                <!-- search starts here -->
+                <div class="container d-flex">
+                    <div class="search ms-5 ps-5">
+                        <div class="input-group mb-3 mt-3" style="width: 600px;">
+                            <input type="text" class="form-control" placeholder="Search" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                            <span class="input-group-text" id="basic-addon2">
+                                <button type="submit" class="btn btn-sm btn-light"><i class="fa-solid fa-magnifying-glass"></i></button></span>
+
+                            <div class="mic mt-2">
+                                <button type="submit" class="btn btn-sm btn-light ms-3 rounded-circle">
+                                    <i class="fa-solid fa-microphone" style="height: 17px;"></i>
+
+                                </button>
+
+                            </div>
+
+                            <!-- video icon starts here -->
+                            
+
+                            <!-- video icon ends here -->
+                        </div>
+                        
+                    </div>
+
+                    <div class="container ac-group-lines d-flex float-end">
+                                <button type="submit" style="width: 46px;" class="btn btn-sm btn-light ms-3 rounded-circle">
+                                    <yt-icon class="style-scope ytd-topbar-menu-button-renderer"><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="pointer-events: none; display: block; width: 100%; height: 100%;">
+                                            <g class="style-scope yt-icon">
+                                                <path d="M14,13h-3v3H9v-3H6v-2h3V8h2v3h3V13z M17,6H3v12h14v-6.39l4,1.83V8.56l-4,1.83V6 M18,5v3.83L22,7v8l-4-1.83V19H2V5H18L18,5 z" class="style-scope yt-icon"></path>
+                                            </g>
+                                        </svg><!--css-build:shady--></yt-icon>
+                                </button>
+
+                                <div class="bell mt-4 ms-3">
+                                    <i class="fa-regular fa-bell"></i>
+                                </div>
+
+
+                                <div class="prof-icon">
+                                <button type="submit" style="width: 46px;" class="btn btn-sm btn-success ms-3 mt-3 rounded-circle">
+                                    <div class="icon-pr rounded-circle  mt-2 mb-2 img-fluid" >D</div>
+                                </button>
+                                </div>
+                            </div>
+
+                </div>
+                <!-- search ends here -->
+
+                <!-- tags starts here -->
+                <div class="container-fluid">
+                    <div class="tags">
+
+                        <button type="button" class="btn btn-dark btn-sm me-2">Primary</button>
+                        <button type="button" class="btn btn-light btn-sm">Primary</button>
+                        <button type="button" class="btn btn-light btn-sm">Primary</button>
+                        <button type="button" class="btn btn-light btn-sm">Primary</button>
+                        <button type="button" class="btn btn-light btn-sm">Primary</button>
+                        <button type="button" class="btn btn-light btn-sm">Primary</button>
+                        <button type="button" class="btn btn-light btn-sm">Primary</button>
+                        <button type="button" class="btn btn-light btn-sm">Primary</button>
+                        <button type="button" class="btn btn-light btn-sm">Primary</button>
+                        <button type="button" class="btn btn-light btn-sm">Primary</button>
+                        <button type="button" class="btn btn-light btn-sm">Primary</button>
+                        <button type="button" class="btn btn-light btn-sm">Primary</button>
+                        <button type="button" class="btn btn-light btn-sm">Primary</button>
+                        <button type="button" class="btn btn-light btn-sm">Primary</button>
+
+
+
+                    </div>
+
+                </div>
+                <!-- tags ends here -->
+
+                <!-- main video starts here -->
+                <div class="container">
+                    <div class="main_video">ramkrishna</div>
+
+                </div>
+
+            </div>
+            <!-- videos container ends here -->
+
         </div>
     </div>
 
 
 
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/all.min.js"></script>
     <script src="js/script.js"></script>
 </body>
+
 </html>
